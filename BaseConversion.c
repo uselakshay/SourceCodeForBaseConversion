3ec8882122cb18a4bfb0ccc22d85345f9ba7508a
uselakshay <151898560+uselakshay@users.noreply.github.com>
2024-10-30 18:16:09 +0530

    Decimal to Binary

--- BaseConversion.c
+++ BaseConversion.c
@@ -7,6 +7,44 @@ int choice()
     scanf("%d", &userchoice);
     return userchoice;
 }
+int askBinary()
+{
+    int bin;
+    printf("Type Binary Number");
+    scanf("%d", &bin);
+    return bin;
+}
+int askDecimal()
+{
+    int dec;
+    printf("Type Decimal Number");
+    scanf("%d", &dec);
+    return dec;
+}
+int convertDecimaltoBinary(int dec)
+{
+    // dec number is 24 into 1010001
+    if (dec == 0){
+        return 0;
+    }else{
+        return (dec % 2)+convertDecimaltoBinary(dec/2);
+    }
+}
+int executingFunction(int userchoice)
+{
+    int bin, dec;
+    switch (userchoice)
+    {
+    case 1:
+        dec = askDecimal();
+        bin = convertDecimaltoBinary(dec);
+        printf("Binary number is %d ",bin);
+        break;
+    default:
+        printf("Error!");
+    }
+}
+
 int main()
 {
     // Structure 1 2 3 4 for choice then exceuting simultaneosly
@@ -14,5 +52,3 @@ int main()
     printf("\n Entered Choice is %d", userchoice);
     return 0;
 }
-
-
