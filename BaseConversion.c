f45064a1c292302f908f5ddf93859a509b9c39fc
uselakshay <151898560+uselakshay@users.noreply.github.com>
2024-10-30 17:58:55 +0530

    1.

--- BaseConversion.c
+++ BaseConversion.c
@@ -1,14 +1,17 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     // Structure 1 2 3 4 for choice then exceuting simultaneosly
     int userchoice = choice();
-    printf("Entered Choice is %d",userchoice);
+    printf("Entered Choice is %d", userchoice);
+    return 0;
 }
 
-int choice(){
+int choice()
+{
     printf("Please Type your choice\n");
     printf("\n Type 1 for Decimal to Binary Num \n Type 2 for Binary to Decimal Num \n Type 3 for Decimal to Hexdecimal \n Type 4 for Hexadecimal to Decimal ");
     int userchoice;
-    scanf("%d",&userchoice);
+    scanf("%d", &userchoice);
     return userchoice;
 }
